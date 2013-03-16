7b39541403e427017d7681efdd12c93dd83f67b0
Max Penet <m@qbits.cc>
2013-03-16 01:16:33 +0100

    create some aliases and cleanup

--- src/qbits/hayt.clj
+++ src/qbits/hayt.clj
@@ -2,18 +2,21 @@
   (:require
    [clojure.core.typed :as t]
    [useful.ns :as uns]
-   [qbits.hayt.cql :as cql]))
+   [qbits.hayt.cql :as cql])
+  (:import [clojure.lang APersistentMap]))
 
-(t/ann ->raw [(clojure.lang.APersistentMap Any Any) -> String])
+(t/def-alias HaytQuery (APersistentMap Any Any))
+(t/def-alias HaytClause (APersistentMap Any Any))
+
+(t/ann ->raw [HaytQuery -> String])
 (def ->raw cql/->raw)
 
-(t/ann ->prepared [(clojure.lang.APersistentMap Any Any) -> '[String '[Any]]])
+(t/ann ->prepared [HaytQuery -> '[String '[Any]]])
 (def ->prepared cql/->prepared)
 
-(->raw {:select :users :columns :*})
+;; (prn (t/cf (->raw {:select :users :columns :*})))
 
-
-
+(t/ann q-> [HaytQuery HaytClause * -> String])
 (defn q->
   "Allows query composition, extending an existing query with new
   clauses"
@@ -22,6 +25,3 @@
 
 (doseq [module '(dsl fns utils)]
   (uns/alias-ns (symbol (str "qbits.hayt." module))))
-
-
-(t/check-ns 'qbits.hayt)
