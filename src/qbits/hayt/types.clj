c9773998b8a8107debbce53b5513158cad0b01e7
Max Penet <m@qbits.cc>
2013-03-16 01:34:57 +0100

    simplify

--- src/qbits/hayt/types.clj
+++ src/qbits/hayt/types.clj
@@ -8,23 +8,21 @@
 (t/def-alias HaytClause (APersistentMap Any Any))
 (t/def-alias CompiledQuery String)
 
-(t/def-alias C*CollType (U (Value :list)
-                           (Value :map)
-                           (Value :set)))
+(t/def-alias C*CollType (U ':list ':map ':set))
 
-(t/def-alias C*Type (U (Value :ascii)
-                       (Value :bigint)
-                       (Value :blob)
-                       (Value :boolean)
-                       (Value :counter)
-                       (Value :decimal)
-                       (Value :double)
-                       (Value :float)
-                       (Value :inet)
-                       (Value :int)
-                       (Value :text)
-                       (Value :timestamp)
-                       (Value :timeuuid)
-                       (Value :uuid)
-                       (Value :varchar)
-                       (Value :varint)))
+(t/def-alias C*Type (U ':ascii
+                       ':bigint
+                       ':blob
+                       ':boolean
+                       ':counter
+                       ':decimal
+                       ':double
+                       ':float
+                       ':inet
+                       ':int
+                       ':text
+                       ':timestamp
+                       ':timeuuid
+                       ':uuid
+                       ':varchar
+                       ':varint))
