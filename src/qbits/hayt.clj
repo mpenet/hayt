70254d8d77d0a22d8bcd6732aa28814fd94c7ac5
Max Penet <m@qbits.cc>
2013-05-06 23:05:57 +0200

    remove deprecated q->

--- src/qbits/hayt.clj
+++ src/qbits/hayt.clj
@@ -6,12 +6,5 @@
 (def ->raw cql/->raw)
 (def ->prepared cql/->prepared)
 
-(defn ^:deprecated q->
-  "Deprecated, just use `merge` or `into`, since it's just maps now.
-Allows query composition, extending an existing query with new
-  clauses"
-  [q & clauses]
-  (into q clauses))
-
 (doseq [module '(dsl fns utils)]
   (uns/alias-ns (symbol (str "qbits.hayt." module))))
