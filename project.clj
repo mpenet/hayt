96ee3d60f64107666b5f68fac43b5f9aaaa7daf8
Max Penet <m@qbits.cc>
2013-09-15 17:05:07 +0200

    back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/hayt "1.2.0"
+(defproject cc.qbits/hayt "1.2.0-SNAPSHOT"
   :description "CQL Query Generation"
   :url "https://github.com/mpenet/hayt"
   :license {:name "Eclipse Public License"
