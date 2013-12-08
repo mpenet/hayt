887196bc6c315a05a32979c3ee66bac7e07335c8
Max Penet <m@qbits.cc>
2013-12-08 18:30:19 +0100

    move to beta2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/hayt "2.0.0-beta1"
+(defproject cc.qbits/hayt "2.0.0-beta2"
   :description "CQL Query Generation"
   :url "https://github.com/mpenet/hayt"
   :license {:name "Eclipse Public License"
