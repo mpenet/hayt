fbf49cf3670dbf3a235477d010f168cc29bdaf22
Max Penet <m@qbits.cc>
2014-08-27 10:59:11 +0200

    back to snapshot

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/mpenet/hayt"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-SNAPSHOT"]
                  [org.flatland/useful "0.10.1"]
                  [org.apache.commons/commons-lang3 "3.1"]]
   :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
