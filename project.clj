aed159ff338bee0bb5f884076c7e604f3fa7ff98
Max Penet <m@qbits.cc>
2014-09-14 13:13:41 +0200

    missing repo

--- project.clj
+++ project.clj
@@ -8,6 +8,12 @@
                  [potemkin "0.3.8"]
                  [org.flatland/useful "0.10.1"]
                  [org.apache.commons/commons-lang3 "3.3.2"]]
+  :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                             :snapshots false
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}}
   :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha2"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
