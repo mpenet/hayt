9b747ce795b637d1052fc349bd6e56a0709ff3e8
Max Penet <m@qbits.cc>
2014-09-28 09:26:10 +0200

    typo

--- src/clj/qbits/hayt/dsl.clj
+++ src/clj/qbits/hayt/dsl.clj
@@ -1,6 +1,6 @@
 (ns qbits.hayt.dsl
   (:refer-clojure :exclude [update])
-  (:require [qbits.common.ns :as uns]))
+  (:require [qbits.commons.ns :as uns]))
 
 (doseq [module '(statement clause)]
   (uns/alias-ns (symbol (str "qbits.hayt.dsl." module))))
