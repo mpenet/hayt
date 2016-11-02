5dd07d48a4c0ab79695bf7de56d806d5c008b28c
Max Penet <m@qbits.cc>
2016-11-02 16:21:36 +0100

    update project.clj

--- project.clj
+++ project.clj
@@ -6,28 +6,11 @@
   :dependencies [[cc.qbits/commons "0.4.6"]
                  [org.apache.commons/commons-lang3 "3.4"]
                  [org.clojure/clojure "1.9.0-alpha14"]]
-  :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
-                             :snapshots false
-                             :releases {:checksum :fail :update :always}}
-                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
-                                       :snapshots true
-                                       :releases {:checksum :fail :update :always}}}
-  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha2"]]}
-             :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
-             :dev  {:dependencies [[clj-time "0.8.0"]
+  :profiles {:dev  {:dependencies [[clj-time "0.8.0"]
                                    [codox "0.8.10"]]}}
-  :aliases {"all" ["with-profile" "dev:dev,1.6:dev,1.7:dev,master"]}
-  :codox {:src-dir-uri "https://github.com/mpenet/hayt/blob/master"
-          :src-linenum-anchor-prefix "L"
-          :output-dir "../hayt-gh/codox"
-          :defaults {:doc/format :markdown}
-          :include [qbits.hayt
-                    qbits.hayt.dsl.statement
-                    qbits.hayt.dsl.clause
-                    qbits.hayt.fns
-                    qbits.hayt.utils
-                    qbits.hayt.codec.joda-time]}
+
+  :codox {:source-uri "https://github.com/mpenet/hayt/blob/master/{filepath}#L{line}"
+          :metadata {:doc/format :markdown}}
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :jvm-opts ^:replace ["-server"]
