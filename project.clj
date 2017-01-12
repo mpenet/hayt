cdbb3bbaef3f2733c4e0fca3454206c7505c0c0e
Max Penet <m@qbits.cc>
2017-01-12 16:04:04 +0100

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/hayt "4.0.0-beta6"
+(defproject cc.qbits/hayt "4.0.0-beta7"
   :description "CQL Query Generation"
   :url "https://github.com/mpenet/hayt"
   :license {:name "Eclipse Public License"
